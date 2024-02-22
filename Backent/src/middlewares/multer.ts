b5694a47761a139f41d8c9739fee9325857b5804
manish <manishkumar60708090@gmail.com>
2024-02-22 14:23:33 +0530

    how to customise the limits of multer filesize and files

--- Backent/src/middlewares/multer.ts
+++ Backent/src/middlewares/multer.ts
@@ -1,5 +1,4 @@
 import { Request } from "express";
-import { Callback } from "mongoose";
 import multer from "multer";
 
 const storage = multer.diskStorage({
@@ -12,32 +11,34 @@ const storage = multer.diskStorage({
   },
 });
 
-// how to set validation 
-const fileFilter = (req, file, cb) => {
-  if(file.mimetype.split("/")[0] === 'image'){
-    cb(null, true)
-
-  }else{
-    cb(new Error("file is not of correct type"), false)
+// how to set validation
+const fileFilter = (req:Request, file:any, cb:any) => {
+  if (file.mimetype.split("/")[0] === "image") {
+    cb(null, true);
+  } else {
+    // cb(new Error("file is not of correct type"), false);
+    cb(new multer.MulterError("LIMIT_UNEXPECTED_FILE"), false)   // if you give this error here mins down the post show input the error field
   }
+};
 
-}
+// single file upload using multer
+export const Singleupload = multer({
+  storage: storage,
+  fileFilter,
+  // An object specifying the size limits of the following optional properties
+  limits: {
+    fileSize:100000,   // 10 mins 10 bites of file this is the limit of two file
+    files:2           // 2 mins, maximum two files are upload
 
+  },
+}).array("file");
 
-// single file upload using multer 
-export const Singleupload = multer({ storage: storage, fileFilter }).single('file')
-
-
-// multiple file upload using multer 
+// multiple file upload using multer
 // export const Singleupload = multer({ storage: storage }).array('file')
 
-
-// multiple image upload 
-// what is name, this is accept of file name 
+// multiple image upload
+// what is name, this is accept of file name
 // export const Singleupload = multer({ storage: storage }).fields([
 //   {name:"avatar", maxCount:10},
 //   {name:'photo', maxCount:10}
 // ])
-
-
-
--- Backent/src/server.ts
+++ Backent/src/server.ts
@@ -1,25 +1,41 @@
-import express from "express";
+import express, { Request, Response, NextFunction } from "express";
 import { PostPdf, getPdf } from "./controllers/pdf.js";
 import { database } from "./database/db.js";
 import { Singleupload } from "./middlewares/multer.js";
 import multer from "multer";
 
-
-import bodyParser  from 'body-parser'
 const app = express();
-app.use(express.json())
+app.use(express.json());
 // app.use(bodyParser.json());
 // app.use(bodyParser.urlencoded({ extended: true }));
 
 
+
 // multer Routes ------------------------------------
 
+app.post("/PostPdf", Singleupload, PostPdf);
+app.use((error: Error, req: Request, res: Response, next: NextFunction) => {
+  if (error instanceof multer.MulterError) {
+    if (error.code === "LIMIT_FILE_SIZE") {
+      return res.json({
+        message: "File To Large",
+      });
+    }
+    if(error.code === "LIMIT_FILE_COUNT"){
+      return res.json({
+        message:"max 2 file are input not more than tw0"
+      })
+    }
 
+    if(error.code === "LIMIT_UNEXPECTED_FILE"){
+      return res.status(400).json({
+        message:"File must be an Image"
+      })
+    }
+  }
+});
 
-
-app.post("/PostPdf", Singleupload, PostPdf )
-
-app.get("/getPdf", getPdf)
+app.get("/getPdf", getPdf);
 
 // api -------------------------------------------
 const port = 5000;
